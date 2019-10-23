1a9c12da2fd3449886b608098cd173c98eba28ca
emeliepetersson <emepet1204@skola.goteborg.se>
2019-10-23 10:46:13 +0200

    add authors and articles arrays to data.php

--- data.php
+++ data.php
@@ -2,3 +2,99 @@
 declare(strict_types=1);
 // This is the file where you can keep your data arrays such as articles and
 // authors.
+
+$authors = [
+    [
+        'ID' => 1,
+        'Full name' => 'Robert Downey Jr.'
+    ],
+    [
+        'ID' => 2,
+        'Full name' => 'Chris Evans'
+    ],
+    [
+        'ID' => 3,
+        'Full name' => 'Scarlett Johansson'
+    ],
+    [
+        'ID' => 4,
+        'Full name' => 'Chris Hemsworth'
+    ],
+    [
+        'ID' => 5,
+        'Full name' => 'Brie Larson'
+    ]
+];
+
+$articles = [
+    [
+        'Title' => 'Jelly jelly tart icing.',
+        'Content' => 'Dragée biscuit chocolate cake. Gummies cookie sugar plum. Candy canes caramels lollipop soufflé brownie cheesecake sesame snaps. Ice cream dragée chupa chups. Sweet roll halvah gummies. Pastry marshmallow dragée liquorice marshmallow danish. Sweet sweet roll marshmallow chocolate bar tart dessert chocolate jujubes. Dragée sweet roll pudding cake. Tootsie roll macaroon croissant cotton candy soufflé sugar plum. Pastry danish jujubes cake biscuit. Lemon drops fruitcake gummi bears pastry wafer sesame snaps donut bonbon powder. Cake biscuit sugar plum tart dessert. Dragée caramels macaroon sesame snaps danish topping. Powder cupcake lollipop ice cream pudding donut.',
+        'Author' => 'Robert Downey Jr.',
+        'Published date' => '2019-05-01',
+        'Like counter' => 24569
+    ],
+    [
+        'Title' => 'Jelly jelly tart icing.',
+        'Content' => 'Dragée biscuit chocolate cake. Gummies cookie sugar plum. Candy canes caramels lollipop soufflé brownie cheesecake sesame snaps. Ice cream dragée chupa chups. Sweet roll halvah gummies. Pastry marshmallow dragée liquorice marshmallow danish. Sweet sweet roll marshmallow chocolate bar tart dessert chocolate jujubes. Dragée sweet roll pudding cake. Tootsie roll macaroon croissant cotton candy soufflé sugar plum. Pastry danish jujubes cake biscuit. Lemon drops fruitcake gummi bears pastry wafer sesame snaps donut bonbon powder. Cake biscuit sugar plum tart dessert. Dragée caramels macaroon sesame snaps danish topping. Powder cupcake lollipop ice cream pudding donut.',
+        'Author' => 'Chris Evans',
+        'Published date' => '2019-09-09',
+        'Like counter' => 1
+    ],
+    [
+        'Title' => 'Jelly jelly tart icing.',
+        'Content' => 'Dragée biscuit chocolate cake. Gummies cookie sugar plum. Candy canes caramels lollipop soufflé brownie cheesecake sesame snaps. Ice cream dragée chupa chups. Sweet roll halvah gummies. Pastry marshmallow dragée liquorice marshmallow danish. Sweet sweet roll marshmallow chocolate bar tart dessert chocolate jujubes. Dragée sweet roll pudding cake. Tootsie roll macaroon croissant cotton candy soufflé sugar plum. Pastry danish jujubes cake biscuit. Lemon drops fruitcake gummi bears pastry wafer sesame snaps donut bonbon powder. Cake biscuit sugar plum tart dessert. Dragée caramels macaroon sesame snaps danish topping. Powder cupcake lollipop ice cream pudding donut.',
+        'Author' => 'Robert Downey Jr.',
+        'Published date' => '2019-02-15',
+        'Like counter' => 598
+    ],
+    [
+        'Title' => 'Jelly jelly tart icing.',
+        'Content' => 'Dragée biscuit chocolate cake. Gummies cookie sugar plum. Candy canes caramels lollipop soufflé brownie cheesecake sesame snaps. Ice cream dragée chupa chups. Sweet roll halvah gummies. Pastry marshmallow dragée liquorice marshmallow danish. Sweet sweet roll marshmallow chocolate bar tart dessert chocolate jujubes. Dragée sweet roll pudding cake. Tootsie roll macaroon croissant cotton candy soufflé sugar plum. Pastry danish jujubes cake biscuit. Lemon drops fruitcake gummi bears pastry wafer sesame snaps donut bonbon powder. Cake biscuit sugar plum tart dessert. Dragée caramels macaroon sesame snaps danish topping. Powder cupcake lollipop ice cream pudding donut.',
+        'Author' => 'Scarlett Johansson',
+        'Published date' => '2019-10-11',
+        'Like counter' => 34
+    ],
+    [
+        'Title' => 'Jelly jelly tart icing.',
+        'Content' => 'Dragée biscuit chocolate cake. Gummies cookie sugar plum. Candy canes caramels lollipop soufflé brownie cheesecake sesame snaps. Ice cream dragée chupa chups. Sweet roll halvah gummies. Pastry marshmallow dragée liquorice marshmallow danish. Sweet sweet roll marshmallow chocolate bar tart dessert chocolate jujubes. Dragée sweet roll pudding cake. Tootsie roll macaroon croissant cotton candy soufflé sugar plum. Pastry danish jujubes cake biscuit. Lemon drops fruitcake gummi bears pastry wafer sesame snaps donut bonbon powder. Cake biscuit sugar plum tart dessert. Dragée caramels macaroon sesame snaps danish topping. Powder cupcake lollipop ice cream pudding donut.',
+        'Author' => 'Chris Hemsworth',
+        'Published date' => '2019-08-30',
+        'Like counter' => 3672
+    ],
+    [
+        'Title' => 'Jelly jelly tart icing.',
+        'Content' => 'Dragée biscuit chocolate cake. Gummies cookie sugar plum. Candy canes caramels lollipop soufflé brownie cheesecake sesame snaps. Ice cream dragée chupa chups. Sweet roll halvah gummies. Pastry marshmallow dragée liquorice marshmallow danish. Sweet sweet roll marshmallow chocolate bar tart dessert chocolate jujubes. Dragée sweet roll pudding cake. Tootsie roll macaroon croissant cotton candy soufflé sugar plum. Pastry danish jujubes cake biscuit. Lemon drops fruitcake gummi bears pastry wafer sesame snaps donut bonbon powder. Cake biscuit sugar plum tart dessert. Dragée caramels macaroon sesame snaps danish topping. Powder cupcake lollipop ice cream pudding donut.',
+        'Author' => 'Brie Larson',
+        'Published date' => '2019-05-08',
+        'Like counter' => 126738
+    ],
+    [
+        'Title' => 'Jelly jelly tart icing.',
+        'Content' => 'Dragée biscuit chocolate cake. Gummies cookie sugar plum. Candy canes caramels lollipop soufflé brownie cheesecake sesame snaps. Ice cream dragée chupa chups. Sweet roll halvah gummies. Pastry marshmallow dragée liquorice marshmallow danish. Sweet sweet roll marshmallow chocolate bar tart dessert chocolate jujubes. Dragée sweet roll pudding cake. Tootsie roll macaroon croissant cotton candy soufflé sugar plum. Pastry danish jujubes cake biscuit. Lemon drops fruitcake gummi bears pastry wafer sesame snaps donut bonbon powder. Cake biscuit sugar plum tart dessert. Dragée caramels macaroon sesame snaps danish topping. Powder cupcake lollipop ice cream pudding donut.',
+        'Author' => 'Brie Larson',
+        'Published date' => '2019-02-12',
+        'Like counter' => 8290
+    ],
+    [
+        'Title' => 'Jelly jelly tart icing.',
+        'Content' => 'Dragée biscuit chocolate cake. Gummies cookie sugar plum. Candy canes caramels lollipop soufflé brownie cheesecake sesame snaps. Ice cream dragée chupa chups. Sweet roll halvah gummies. Pastry marshmallow dragée liquorice marshmallow danish. Sweet sweet roll marshmallow chocolate bar tart dessert chocolate jujubes. Dragée sweet roll pudding cake. Tootsie roll macaroon croissant cotton candy soufflé sugar plum. Pastry danish jujubes cake biscuit. Lemon drops fruitcake gummi bears pastry wafer sesame snaps donut bonbon powder. Cake biscuit sugar plum tart dessert. Dragée caramels macaroon sesame snaps danish topping. Powder cupcake lollipop ice cream pudding donut.',
+        'Author' => 'Chris Evans',
+        'Published date' => '2018-07-09',
+        'Like counter' => 24
+    ],
+    [
+        'Title' => 'Jelly jelly tart icing.',
+        'Content' => 'Dragée biscuit chocolate cake. Gummies cookie sugar plum. Candy canes caramels lollipop soufflé brownie cheesecake sesame snaps. Ice cream dragée chupa chups. Sweet roll halvah gummies. Pastry marshmallow dragée liquorice marshmallow danish. Sweet sweet roll marshmallow chocolate bar tart dessert chocolate jujubes. Dragée sweet roll pudding cake. Tootsie roll macaroon croissant cotton candy soufflé sugar plum. Pastry danish jujubes cake biscuit. Lemon drops fruitcake gummi bears pastry wafer sesame snaps donut bonbon powder. Cake biscuit sugar plum tart dessert. Dragée caramels macaroon sesame snaps danish topping. Powder cupcake lollipop ice cream pudding donut.',
+        'Author' => 'Scarlett Johansson',
+        'Published date' => '2018-12-04',
+        'Like counter' => 3840
+    ],
+    [
+        'Title' => 'Jelly jelly tart icing.',
+        'Content' => 'Dragée biscuit chocolate cake. Gummies cookie sugar plum. Candy canes caramels lollipop soufflé brownie cheesecake sesame snaps. Ice cream dragée chupa chups. Sweet roll halvah gummies. Pastry marshmallow dragée liquorice marshmallow danish. Sweet sweet roll marshmallow chocolate bar tart dessert chocolate jujubes. Dragée sweet roll pudding cake. Tootsie roll macaroon croissant cotton candy soufflé sugar plum. Pastry danish jujubes cake biscuit. Lemon drops fruitcake gummi bears pastry wafer sesame snaps donut bonbon powder. Cake biscuit sugar plum tart dessert. Dragée caramels macaroon sesame snaps danish topping. Powder cupcake lollipop ice cream pudding donut.',
+        'Author' => 'Chris Hemsworth',
+        'Published date' => '2018-03-28',
+        'Like counter' => 162784
+    ]
+];
