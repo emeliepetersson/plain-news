415dfdac99a5a0f9b23b3e5ceedcc18b2bdc81bb
emeliepetersson <emepet1204@skola.goteborg.se>
2019-10-23 11:48:36 +0200

    add function dateCompare

--- functions.php
+++ functions.php
@@ -2,3 +2,15 @@
 declare(strict_types=1);
 // This is the file where you can keep all your functions. Remember to NOT
 // execute/run any functions in this file. Keep it dumb.
+
+//https://stackoverflow.com/questions/2910611/php-sort-a-multidimensional-array-by-element-containing-date
+function dateCompare($a, $b)
+{
+    $t1 = strtotime($a['Published date']);
+    $t2 = strtotime($b['Published date']);
+    return $t1 - $t2;
+}    
+
+
+
+
--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
+
+require __DIR__.'/data.php';
+require __DIR__.'/functions.php';
+
+usort($articles, 'dateCompare');
+
+//echo "<pre>";
+//die(var_dump($articles));
+//echo "</pre>";
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,5 +22,16 @@
         <title></title>
     </head>
     <body>
+        <h1>News feed</h1>
+
+        <!-- Detta ska bli en for loop som skapar en artikel per $articles arrayn, plus $articles['author']===$authors['fullname'] ska skriva ut full name och ID-->
+        <article>
+        <h1>Title</h1>
+        <p>Content</p>
+        <footer>
+        Author ID | Published date | <img src="like.png" alt="thumbs up like icon"> Like counter (the number of likes a news feed item has received)
+        </footer>
+        </article>
+
     </body>
 </html>
