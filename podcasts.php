f10688926e29140c0a9b171ec5a90bc09ac2cfe3
emeliepetersson <emepet1204@skola.goteborg.se>
2019-10-28 11:27:01 +0100

    clean up code and add comments

--- podcasts.php
+++ podcasts.php
@@ -1,7 +1,4 @@
 <?php
-// This is the file where you can keep your HTML markup. We should always try to
-// keep us much logic out of the HTML as possible. Put the PHP logic in the top
-// of the files containing HTML or even better; in another PHP file altogether.
 
 require __DIR__.'/header.php';
 
